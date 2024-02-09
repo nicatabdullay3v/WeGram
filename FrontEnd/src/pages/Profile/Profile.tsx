119505eb9304ddeca9a74f0d806b5949492e8d85
nicatabdullay3v <nicat.neon@bk.ru>
2024-02-09 17:58:44 +0400

    Profile Posts Detail

--- FrontEnd/src/pages/Profile/Profile.tsx
+++ FrontEnd/src/pages/Profile/Profile.tsx
@@ -8,24 +8,26 @@ import { useEffect, useState } from "react"
 import { AiOutlineClose } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { motion } from "framer-motion"
 import Pagination from '@mui/material/Pagination';
 import 'cookie-store';
 import RecomendedUsers from "../../components/Home/RecomendedUsers/RecomendedUsers";
-import { BsHeart } from "react-icons/bs";
 import { FaComment } from "react-icons/fa";
 import { HiHeart } from "react-icons/hi2";
+import { BsHeart } from "react-icons/bs";
 const Profile = () => {
     const navigate = useNavigate()
     const [modal, setModal] = useState(false)
     const [title, setTitle] = useState("")
+    const [postId, setpostId] = useState('')
     const [likes, setlikes] = useState([])
+    const [postModal, setpostModal] = useState(false)
     const [currentPage, setCurrentPage] = useState(1);
     const postsPerPage = 8;
     const [file, setFile] = useState<File | undefined>()
     const dispatch = useDispatch<AppDispatch>()
     const LocalUserID: string = JSON.parse(localStorage.getItem("user-info") || "{}")._id
     const user = useSelector((state: RootState) => state.users.user)
+    const users = useSelector((state: RootState) => state.users.users)
     useEffect(() => {
 
         if (localStorage.getItem("user-info")) {
@@ -69,10 +71,42 @@ const Profile = () => {
     const paginate = (pageNumber: number) => {
         setCurrentPage(pageNumber);
     };
+
+    const detailPost = user?.posts.find((x) => x.id == postId)
     return (
         <>
             <NavBar />
             <div id="user_profile">
+                {postModal ? <div className="post_modal">
+                    <div className="post_modal_left">
+                        <div className="post">
+                            <img src={`http://localhost:3001/${detailPost?.img}`} alt="" />
+                        </div>
+                        <div className="post_modal_down">
+                            <div onClick={() => {
+                                axios.patch(`http://localhost:3001/api/users/${LocalUserID}/posts/${postId}`, {
+                                    likes: [...detailPost?.likes!, { _id: user?._id }]
+                                }).then(() => {
+                                    dispatch(getAllData())
+                                    dispatch(getUserById(LocalUserID))
+                                })
+                            }} className="post_heart">
+                                <BsHeart className="icon" />
+                            </div>
+                            <div className="post_comment">
+                                <FaComment className="icon" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="post_modal_right">
+                        <div className="post_comments">
+                            {detailPost?.comments.map((x: { comment: string, _id: string }) => {
+                                const commentUser = users.find((z) => z._id == x._id)
+                                return <li>{commentUser?.username ? commentUser?.username : user?.username}: {x.comment}</li>
+                            })}
+                        </div>
+                    </div>
+                </div> : null}
                 {modal ? <div className="modal">
                     <div style={{ textAlign: 'end' }} className="close">
                         <AiOutlineClose onClick={() => {
@@ -133,7 +167,9 @@ const Profile = () => {
                                 <div className="post-cards">
                                     {currentPosts?.map((elem: any) => {
                                         return <div onClick={() => {
-                                            // navigate(`/user/${LocalUserID}/posts/${elem.id}`)
+                                            setpostModal(true)
+                                            setpostId(elem.id)
+
                                         }} key={elem._id} className="post_card">
                                             <div className="post">
                                                 <img src={`http://localhost:3001/${elem.img}`} alt="" />
